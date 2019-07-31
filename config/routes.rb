ec1703d9cd130de2cc6725a89cf2f9bbe622b274
Alexander <alexandernino98@gmail.com>
2019-07-31 14:28:44 +0200

    rails practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :comments
   root 'welcome#home'
   
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
